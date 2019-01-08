1c3b07a8d8499d510ec03abd6038ca1d026b1a49
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2019-01-08 17:15:19 +0200

    added structure without content

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# responsive-web
+# responsive-web without framework
 responsive ws from 0
+
+Structure SASS folder:
+base        for basic styles and CSS reset
+config      for configuration file
+mixins      for mixins and functions
+modules     for reusable elements not tired to any particular page 
+pages       for styles that are specific to a page or large section or the website
+strucrure   for structural elements like the main header and footer
+
+Using Gulp plugins
+github -> Pestov/essential-gulp-plugins
+gulp-useref
+gulp-uglify
+gulp-responsive
+gulp-uncss
+gulp-htmlmin
+
+formspree.io
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
   sourcemaps = require('gulp-sourcemaps'),
   autoprefixer = require('gulp-autoprefixer'),
   imagemin = require('gulp-imagemin'),
+  wait = require('gulp-wait'),
   browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
@@ -12,8 +13,10 @@ gulp.task('default', function() {
 
 gulp.task('css', function() {
   return gulp.src('src/sass/**/*.scss')
+    .pipe(wait(1500))
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+    // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('dist/css'))
@@ -27,7 +30,7 @@ gulp.task('images', function() {
 });
 
 gulp.task('copy', function() {
-  return gulp.src('src/*.html')
+  return gulp.src('src/**/*.+(html|js)')
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
 });
@@ -38,5 +41,28 @@ gulp.task('browserSync', function() {
 
 gulp.task('watch', ['browserSync', 'css'], function() {
   gulp.watch('src/sass/**/*.scss', ['css']);
+  gulp.watch('src/**/*.+(html|js)', ['copy']);
+});
+
+
+// Static Server + watching scss/html files
+gulp.task('serve', function() {
+
+  browserSync.init({
+      server: "./dist"
+  });
+
+  gulp.watch("src/sass/**/*.scss", ['sass']);
+  // gulp.watch("src/*.html").on('change', browserSync.reload);
   gulp.watch('src/*.html', ['copy']);
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("src/sass/**/*.scss")
+    .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch2', ['sass', 'serve']);
